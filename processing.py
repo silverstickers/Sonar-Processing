8355c5106a4c9ef4941e7794c5469490285e8e78
Silverstickers <silverstickers@t-online.de>
2022-05-01 12:17:11 +0200

    Ignore Serial message errors. Store some waveforms

--- processing.py
+++ processing.py
@@ -1,3 +1,4 @@
+from socket import timeout
 from textwrap import wrap
 import serial
 import time
@@ -20,8 +21,11 @@ if __name__ == '__main__':
     plt.ion()
     plt.show(block=False)
 
+    filesWritten = 0
+    saveWaveforms = True
 
-    with serial.Serial(port='COM4', baudrate=115200, timeout=4) as arduino:
+
+    with serial.Serial(port='COM4', baudrate=115200, timeout=5.0) as arduino:
         def write_read(x):
             arduino.write(bytes(x, 'utf-8'))
             time.sleep(0.05)
@@ -29,65 +33,64 @@ if __name__ == '__main__':
             return data
 
         while True:
-            zeroCount = 0
+            count = 0
+            pattern = [1, 200, 31, 41]
+            patternPosition = 0
             while True:
                 value = arduino.read(1)[0]
-                if value == 1:
-                    zeroCount += 1
+                if value == pattern[patternPosition]:
+                    count += 1
                 else:
-                    zeroCount = 0
-                if zeroCount > 48:
-                    print("Looking for 50 ones in a row, found: {} ones in a row".format(zeroCount))
-                if zeroCount == 50:
-                    break
+                    count = 0
+                    patternPosition = 0
+                if count > 24:
+                    print("Found {} {} in a row".format(count, pattern[patternPosition]))
+                    patternPosition += 1
+                    count = 0
+                    if patternPosition == len(pattern):
+                        break
             
-            message = arduino.readline()
-            print(message.decode("utf-8"))
-            message = arduino.readline()
-            print(message.decode("utf-8"))
-            message = arduino.readline()
-            print(message.decode("utf-8"))
+            try:
+                message = arduino.readline()
+                print(message.decode("utf-8"))
+                
+                bytes_per_array = int.from_bytes(arduino.read(4), byteorder='little')
+                print("Bytes per array: {}".format(bytes_per_array))
+                start_time = timeit.default_timer()
+                array1 = arduino.read(bytes_per_array)
+                array2 = arduino.read(bytes_per_array)
+                elapsed = timeit.default_timer() - start_time
+
+                
+                print("Timed the serial read. Elapsed time: {:2.3f} ms".format(elapsed * 1000))
+                
+                
+                start_time = time.perf_counter_ns()
+                array1 = convertToInt(array1)
+                array2 = convertToInt(array2)
+                elapsed = time.perf_counter_ns() - start_time
+                print("Converted {} bytes into {} integers in {:2.3f} ms".format(bytes_per_array*2, bytes_per_array, elapsed * 1e-6))
+                
+
+                xarr = range(len(array1))
+
+                if (max(array1) < 4100 and max(array2) < 4100):
+                    ax1.clear()
+                    ax1.plot(xarr, array1)
+                    ax1.plot(xarr, array2)
+
+                    plt.pause(0.001)
+
+                if filesWritten < 10:
+                    with open('waveform_{}'.format(filesWritten), 'w') as f:
+                        for a, b in zip(array1, array2):
+                            f.write("{} {}\n".format(a, b))
+                    filesWritten += 1
+
+                message = arduino.readline()
+                print(message.decode("utf-8"))
+
+                print("\n")
             
-            bytes_per_array = int.from_bytes(arduino.read(4), byteorder='little')
-            print("Bytes per array: {}".format(bytes_per_array))
-            print(arduino.readline().decode("utf-8"))
-            start_time = timeit.default_timer()
-            array1 = arduino.read(bytes_per_array)
-            array2 = arduino.read(bytes_per_array)
-            elapsed = timeit.default_timer() - start_time
-
-            
-
-            print("Timed the serial read of 20kB. Elapsed time: {:2.3f} ms".format(elapsed * 1000))
-            # print(int.from_bytes(array1[0:2], byteorder='little'))
-            # print(int.from_bytes(array1[2:4], byteorder='little'))
-            # print(int.from_bytes(array2[0:2], byteorder='little'))
-            # print(int.from_bytes(array2[2:4], byteorder='little'))
-            
-            
-            
-            start_time = time.perf_counter_ns()
-            array1 = convertToInt(array1)
-            array2 = convertToInt(array2)
-            elapsed = time.perf_counter_ns() - start_time
-            print("Converted {} bytes into {} integers in {:2.3f} ms".format(bytes_per_array*2, bytes_per_array, elapsed * 1e-6))
-
-            print(array1[0])
-            print(array1[1])
-            print(array2[0])
-            print(array2[1])
-
-            
-
-            xarr = range(len(array1))
-
-            ax1.clear()
-            ax1.plot(xarr, array1)
-            ax1.plot(xarr, array2)
-
-            plt.pause(0.001)
-
-            message = arduino.readline()
-            print(message.decode("utf-8"))
-
-            print("\n\n")
+            except UnicodeDecodeError:
+                pass
